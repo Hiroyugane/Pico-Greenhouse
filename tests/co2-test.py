29d491b3719091d99a8f9e3d13265ca85abd2d3a
Dennis <Dennis@local.de>
2026-02-16 02:03:27 +0100

    first CO2 Reading test (Senseair S8-004-0-0053)

---
+++ tests/co2-test.py
@@ -0,0 +1,29 @@
+from machine import UART, Pin
+from time import sleep
+
+
+def main():
+    uart1 = UART(0, baudrate=9600, tx=Pin(0), rx=Pin(1), timeout=500)
+    uart1.init(9600, bits=8, parity=None, stop=1)
+    uart1.flush()
+    sleep(1)
+    
+    while True:
+        uart1.flush()
+        sequence_to_send = b'\xFE\x44\x00\x08\x02\x9F\x25'
+        uart1.write(sequence_to_send)
+        sleep(1)
+                
+        if uart1.any():
+            resp = uart1.read(7)
+            if resp is not None and len(resp) >= 5:
+                high = resp[3]
+                low = resp[4]
+                co2 = high * 256 + low
+                
+                print(co2)
+        else:
+            print("Nothing to read...")
+
+if __name__ == "__main__":
+    main()
